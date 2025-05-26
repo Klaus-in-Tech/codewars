66b22daee6940241f167cc05ab70e322857a8910
Klaus-in-Tech <kkallan07@gmail.com>
2025-05-26 09:00:31 +0300

    Sum of Odd Cubed Numbers

--- 7kyu/sum_of_odd_cubed_numbers.py
+++ 7kyu/sum_of_odd_cubed_numbers.py
@@ -7,12 +7,15 @@ Note: Booleans should not be considered as numbers.
 """
 
 def cube_odd(arr):
-    #Identifying non integer values within an array 
 
+    #Identifying non integer values within an array 
     for i in arr:
         if (not isinstance(i,int)):
             return None
-    
+    #Identifying boolean values within an array
+    for i in arr:
+        if isinstance(i, bool):
+            return None
     #Identifying the odd numbers with an array
     odd_arr = []
     for i in arr:
