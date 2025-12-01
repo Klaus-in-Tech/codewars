f8945910bab1399e9f90d687c1c8e9456c45f63b
Kakooza Allan Klaus <kkallan07@gmail.com>
2025-12-01 00:02:54 -0700

    So Many Permutations!

---
+++ 4kyu/permutations.py
@@ -0,0 +1,34 @@
+"""
+In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+
+Create as many "shufflings" as you can!
+
+Examples:
+
+With input 'a':
+Your function should return: ['a']
+
+With input 'ab':
+Your function should return ['ab', 'ba']
+
+With input 'abc':
+Your function should return ['abc','acb','bac','bca','cab','cba']
+
+With input 'aabb':
+Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+Note: The order of the permutations doesn't matter.
+
+Good luck!
+
+
+"""
+
+import itertools
+
+def permutations(s):
+    all_permutations = itertools.permutations(s)
+    list_permutations = ["".join(p) for p in all_permutations]
+    return set(list_permutations)
+
+# Example usage:
+print(permutations('aabb'))
