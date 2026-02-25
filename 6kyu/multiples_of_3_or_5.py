71418de2d55502e1989c20b355d3b05bf78c2201
Klaus-in-Tech <kkallan07@gmail.com>
2026-02-25 00:16:40 -0700

    Multiples of 3 or 5

---
+++ 6kyu/multiples_of_3_or_5.py
@@ -0,0 +1,17 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If a number is a multiple of both 3 and 5, only count it once.
+"""
+def solution(number):
+    multiples = [num for num in range(number) if num % 3 == 0 or num % 5 == 0]
+    return sum(multiples)
+
+
+print(solution(10))  # Output: 23
+print(solution(20))  # Output: 78
+print(solution(-5))  # Output: 0
