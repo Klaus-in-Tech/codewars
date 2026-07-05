75b205a7ac55d3e076f7fe4449ad0557f86857b7
Kakooza Allan Klaus <kkallan07@gmail.com>
2026-07-05 14:41:03 +0300

    Added comments on the functions

--- 4kyu/rbac.py
+++ 4kyu/rbac.py
@@ -16,13 +16,22 @@ permissions = [
       { "role": "rider", "name": "scooters near me", "active": True },
     ]
 
-    
+
+# @rtype: list of strings
+# @returns: a list of all the active permission names that the user with the corresponding user_id has
+# @note: The order in which the permission names are returned is not important
+# @example: listPermissions(1) ➡ ["View Orders", "Block User Account"] (purchased widgets not included since it is not active)    
 def list_permissions(user_id):
     user = next((u for u in users if u.get("id") == user_id), f"No user with {user_id}")
     roles =(user.get("roles", f"No roles with {user_id}"))
     result = [p.get('name') for p in permissions if p.get('active') and p.get('role') in roles]
     return result
-    
+
+# @rtype: boolean value
+# @returns: true or false based on if the user with the corresponding user_id has the role that corresponds with the specific permission_name and that particular permission is active
+# @example: Example (Based on data from graphic)
+# checkPermitted("scooters near me", 2) ➡ true
+# checkPermitted("scooters near me", 1) ➡ false    
 def check_permitted(permission_name, user_id):
     user = next((u for u in users if u.get("id")==user_id),f"No user with id{user_id}")
     roles = (user.get("roles"),[])
